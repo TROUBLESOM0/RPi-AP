ed409a4f8556e267afe877e5501b739c02b86f4f
TROUBLESOM0 <80598840+TROUBLESOM0@users.noreply.github.com>
2025-02-06 22:40:11 -0600

    added login data

--- sta-ap/check-net.sh
+++ sta-ap/check-net.sh
@@ -14,6 +14,7 @@ then echo -e "${Error}ERROR${Off} Must be run as sudo or root"
 exit 1
 fi
 #
+#
 # Begin Script
 #
 ping -c 1 8.8.8.8 &> /dev/null
@@ -23,21 +24,40 @@ then
 # If ping fails
 echo "No connection to Google, trying Cloudflare"
 ping -c 1 1.1.1.1 &> /dev/null
-if [ $? -ne 0 ]
-then
-echo "No connection to Cloudflare."
-echo "Starting sta-ap"
-exit 1
-# If network is connected
-else echo "successful ping to Cloudflare."
-echo "Starting subcloud"
-echo "run test scritp"
-./test
-fi
-else
-echo "successful ping to Google"
-echo "Starting subcloud"
-echo "run test script"
-./test
+  if [ $? -ne 0 ]
+  then
+  echo "No connection to Cloudflare, trying login.php"
+    if [[ ! -f /var/www/html/login.php ]]
+    then
+    echo "Starting sta-ap"
+    echo "#sudo ./sta-ap.start"
+    exit 0
+    else echo "login.php exists. Wifi Login should be running"
+      if [[ -f /var/www/html/login.data ]]
+      then
+      echo "login.data exists"
+      echo "run sta.stop+++++++++++++++"
+      else echo "login.data no exists, but login.php should be running...waiting for input"
+      exit 0
+      fi
+    fi
+  # If network is connected
+  else echo "successful ping to Cloudflare."
+  echo "checking if need to run sta-ap.stop"
+    if [[ -f /var/www/html/login.data ]]
+    then
+    echo "login.data exists"
+    echo "run sta.stop--------------------"
+    else exit 0
+    fi
+  fi
+else echo "successful ping to Google."
+echo "checking if need to run sta-ap.stop"
+  if [[ -f /var/www/html/login.data ]]
+  then
+  echo "login.data exists"
+  echo "run sta.stop *********************"
+  else exit 0
+  fi
 fi
 exit 0
