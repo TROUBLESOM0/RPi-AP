88cccaf52dbdff4cba0f67b60a8bdf53c7c93a7f
TROUBLESOM0 <80598840+TROUBLESOM0@users.noreply.github.com>
2026-06-08 13:55:33 -0500

    Update Installunzip.sh

--- deploy/Installunzip.sh
+++ deploy/Installunzip.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
+# check if unzip is installed
+if type unzip &>/dev/null
+then echo -e "\nunzip already installed\n"
+: # continues script
+else
+echo -e "\nUnzip is not installed"
 # installs unzip
 echo "Installing unzip"
 apt install unzip -y -qq > /dev/null
 sleep 1
 
-if type unzip &>/dev/null
-then echo -e "Installed unzip\n"
-return
-else
-echo -e "${Error}ERROR${Off} unzip installation failed. Try installing manually with \"sudo apt install unzip\" and run again"
-exit 1
+  if type unzip &>/dev/null
+  then echo -e "Installed unzip\n"
+  :
+  else
+  echo -e "${Error}ERROR${Off} unzip installation failed. Try installing manually with \"sudo apt install unzip\" and run again"
+  exit 1
+  fi
+
 fi
