48bfccefb934f7b7a2d25b756e04981a8666fd5a
TROUBLESOM0 <80598840+TROUBLESOM0@users.noreply.github.com>
2026-06-08 13:59:27 -0500

    Update Installwget.sh

--- deploy/Installwget.sh
+++ deploy/Installwget.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
+# check if wget is installed
+if type wget &>/dev/null
+then echo -e "\nwget already installed\n"
+: # continues script
+else
+echo -e "\nwget is not installed"
 echo "Installing wget"
 apt install wget -y -qq > /dev/null
 sleep 1
 
-if type wget &>/dev/null
-then echo -e "Installed wget\n"
-return
-else
-echo -e "${Error}ERROR${Off} wget installation failed. Try installing manually with \"sudo apt install wget\" and run again"
-exit 1
+  if type wget &>/dev/null
+  then echo -e "Installed wget\n"
+  :
+  else
+  echo -e "${Error}ERROR${Off} wget installation failed. Try installing manually with \"sudo apt install wget\" and run again"
+  exit 1
+  fi
+  
 fi
