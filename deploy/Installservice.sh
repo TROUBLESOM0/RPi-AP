19518f7b7ae004a8c6441bcb72d6df85ef63cff5
TROUBLESOM0 <80598840+TROUBLESOM0@users.noreply.github.com>
2026-06-08 15:06:06 -0500

    Create Installservice.sh

---
+++ deploy/Installservice.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# check service file exists
+if [[ -f /etc/systemd/system/$service ]]
+then echo "removing existing service"
+systemctl disable $service
+else :
+fi
+
+if [[ ! -f $stadir/$req/$service ]]
+then echo -e "${Error}ERROR${Off} $service is missing!\n"
+exit 1
+else
+chown root:root $stadir/$req/$service
+chmod u+rwx,g+rx,o+r $stadir/$req/$service
+sleep 2
+ln -s $stadir/$req/$service /etc/systemd/system/$service
+sleep 1
+echo "enabling service..."
+systemctl enable $service
+# check for errors on service
+echo "checking for errors..."
+  if systemctl is-enabled "$service" &>/dev/null
+  then echo -e "service is enabled\n"
+  else
+  echo "There was an issue configuring the service '$service'!"
+  echo "Run Uninstall script"
+  echo -e "Then try re-installing\n"
+  exit 1
+  fi
+fi
